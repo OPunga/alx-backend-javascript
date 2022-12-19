2ebf1425c026d8b19d5fd71f2b6e5c460fbd29f9
OPunga <ronnyopunga96@gmail.com>
2022-12-20 00:47:51 +0300

    rest-para

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,7 +1,3 @@
-export default function returnHowManyArguments(..theArgs) {
-	let total = 0;
-	for (const args in theArgs){
-		total += args;
-	}
-	return total;
+export default function returnHowManyArguments(...args) {
+  return args.length;
 }
