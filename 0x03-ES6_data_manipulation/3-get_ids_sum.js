caf699f848a430a91d3dd0c53ae1ec99bd444423
OPunga <ronnyopunga96@gmail.com>
2023-01-04 01:07:29 +0300

    reduce

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,3 @@
-expot default function getStudentIdsSum(lst) {
-  return lst.reduce((counter, currentObj) => counter + currentObj.id, 0);
+export default function getStudentIdsSum(lt) {
+  return lt.reduce((counter, currentObj) => counter + currentObj.id, 0);
 }
