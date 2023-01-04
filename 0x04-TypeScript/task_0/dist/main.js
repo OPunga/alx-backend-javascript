1140281c1c922d0add6b9cd5330edc3aadf106b6
OPunga <ronnyopunga96@gmail.com>
2023-01-04 21:41:54 +0300

    interface

---
+++ 0x04-TypeScript/task_0/dist/main.js
@@ -0,0 +1,33 @@
+var student1 = {
+    firstName: 'jane',
+    lastName: 'Achieng',
+    age: 30,
+    location: 'kisumu',
+};
+var student2 = {
+    firstName: 'mane',
+    lastName: 'otieno',
+    age: 23,
+    location: 'nairobi',
+};
+var studentsList = [student1, student2];
+var table = document.createElement('table');
+var tbody = document.createElement('tbody');
+table.style.background = "lightgray";
+table.appendChild(tbody);
+studentsList.forEach(function (student) {
+    var row = document.createElement('tr');
+    var nameCell = document.createElement('td');
+    var locationCell = document.createElement('td');
+    nameCell.textContent = student.firstName;
+    locationCell.textContent = student.location;
+    nameCell.style.border = "2px solid white";
+    locationCell.style.border = "2px solid white";
+    nameCell.style.padding = "4px";
+    locationCell.style.padding = "4px";
+    row.appendChild(nameCell);
+    row.appendChild(locationCell);
+    tbody.appendChild(row);
+});
+document.body.appendChild(table);
+//# sourceMappingURL=main.js.map
